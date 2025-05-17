52cec0898d90f96c50c126e3a123178fff0c6782
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-05-17 17:29:00 +0300

    New: Special Seasons filtering for Wanted Missing

--- src/Sonarr.Api.V5/Wanted/MissingController.cs
+++ src/Sonarr.Api.V5/Wanted/MissingController.cs
@@ -26,7 +26,7 @@ public class MissingController : EpisodeControllerWithSignalR
 
     [HttpGet]
     [Produces("application/json")]
-    public Ok<PagingResource<EpisodeResource>> GetMissingEpisodes([FromQuery] PagingRequestResource paging, bool monitored = true, [FromQuery] MissingSubresource[]? includeSubresources = null)
+    public Ok<PagingResource<EpisodeResource>> GetMissingEpisodes([FromQuery] PagingRequestResource paging, bool monitored = true, bool includeSpecials = true, [FromQuery] MissingSubresource[]? includeSubresources = null)
     {
         var pagingResource = new PagingResource<EpisodeResource>(paging);
         var pagingSpec = pagingResource.MapToPagingSpec<EpisodeResource, Episode>(
@@ -51,7 +51,7 @@ public class MissingController : EpisodeControllerWithSignalR
         var includeSeries = includeSubresources.Contains(MissingSubresource.Series);
         var includeImages = includeSubresources.Contains(MissingSubresource.Images);
 
-        var resource = pagingSpec.ApplyToPage(_episodeService.EpisodesWithoutFiles, v => MapToResource(v, includeSeries, false, includeImages));
+        var resource = pagingSpec.ApplyToPage(spec => _episodeService.EpisodesWithoutFiles(spec, includeSpecials), v => MapToResource(v, includeSeries, false, includeImages));
 
         return TypedResults.Ok(resource);
     }
