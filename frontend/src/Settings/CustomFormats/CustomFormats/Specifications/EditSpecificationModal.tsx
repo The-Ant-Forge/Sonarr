28d701dfd001207a235e2336090a62c72d817f72
Mark McDowall <mark@mcdowall.ca>
2026-05-13 18:56:48 -0700

    Use react-query for Custom Formats

--- frontend/src/Settings/CustomFormats/CustomFormats/Specifications/EditSpecificationModal.tsx
+++ frontend/src/Settings/CustomFormats/CustomFormats/Specifications/EditSpecificationModal.tsx
@@ -1,15 +1,12 @@
-import React, { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+import React from 'react';
 import Modal from 'Components/Modal/Modal';
 import { sizes } from 'Helpers/Props';
-import { clearPendingChanges } from 'Store/Actions/baseActions';
 import EditSpecificationModalContent, {
   EditSpecificationModalContentProps,
 } from './EditSpecificationModalContent';
 
-const section = 'settings.customFormatSpecifications';
-
-interface EditSpecificationModalProps extends EditSpecificationModalContentProps {
+interface EditSpecificationModalProps
+  extends EditSpecificationModalContentProps {
   isOpen: boolean;
 }
 
@@ -18,19 +15,11 @@ function EditSpecificationModal({
   onModalClose,
   ...otherProps
 }: EditSpecificationModalProps) {
-  const dispatch = useDispatch();
-
-  const handleModalClose = useCallback(() => {
-    dispatch(clearPendingChanges({ section }));
-
-    onModalClose();
-  }, [dispatch, onModalClose]);
-
   return (
-    <Modal size={sizes.MEDIUM} isOpen={isOpen} onModalClose={handleModalClose}>
+    <Modal size={sizes.MEDIUM} isOpen={isOpen} onModalClose={onModalClose}>
       <EditSpecificationModalContent
         {...otherProps}
-        onModalClose={handleModalClose}
+        onModalClose={onModalClose}
       />
     </Modal>
   );
--- frontend/src/Store/Actions/settingsActions.js
+++ frontend/src/Store/Actions/settingsActions.js
@@ -1,13 +1,9 @@
 import { handleThunks } from 'Store/thunks';
 import createHandleActions from './Creators/createHandleActions';
-import customFormats from './Settings/customFormats';
-import customFormatSpecifications from './Settings/customFormatSpecifications';
 import delayProfiles from './Settings/delayProfiles';
 import downloadClientOptions from './Settings/downloadClientOptions';
 import downloadClients from './Settings/downloadClients';
 
-export * from './Settings/customFormatSpecifications.js';
-export * from './Settings/customFormats';
 export * from './Settings/delayProfiles';
 export * from './Settings/downloadClients';
 export * from './Settings/downloadClientOptions';
@@ -22,8 +18,6 @@ export const section = 'settings';
 
 export const defaultState = {
   advancedSettings: false,
-  customFormatSpecifications: customFormatSpecifications.defaultState,
-  customFormats: customFormats.defaultState,
   delayProfiles: delayProfiles.defaultState,
   downloadClients: downloadClients.defaultState,
   downloadClientOptions: downloadClientOptions.defaultState,
@@ -35,8 +29,6 @@ export const persistState = ['settings.importListExclusions.pageSize'];
 // Action Handlers
 
 export const actionHandlers = handleThunks({
-  ...customFormatSpecifications.actionHandlers,
-  ...customFormats.actionHandlers,
   ...delayProfiles.actionHandlers,
   ...downloadClients.actionHandlers,
   ...downloadClientOptions.actionHandlers,
@@ -47,8 +39,6 @@ export const actionHandlers = handleThunks({
 
 export const reducers = createHandleActions(
   {
-    ...customFormatSpecifications.reducers,
-    ...customFormats.reducers,
     ...delayProfiles.reducers,
     ...downloadClients.reducers,
     ...downloadClientOptions.reducers,
